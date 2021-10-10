050b5abd3019716021ea5cbd13735ed764c3dd78
narumij <narumij@gmail.com>
2021-10-10 15:20:59 +0900

    framework build script

---
+++ XCFramework.sh
@@ -0,0 +1,2 @@
+xcodebuild -project XCFrameworks/chull/chull.xcodeproj -target Aggregate -configuration Release clean build
+xcodebuild -project XCFrameworks/chull/chull.xcodeproj -target Aggregate -configuration Relase clean
